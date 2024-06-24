9428a606bfd5187c12a4ca2a089312a496e256f4
AbhishekAhirwar1508 <abhishek.ahirwar@rapidops.co>
2024-06-24 18:14:26 +0530

    Reference code from android ar model viewer

--- android/src/main/kotlin/com/example/flutter_ar/FlutterArWrapper.kt
+++ android/src/main/kotlin/com/example/flutter_ar/FlutterArWrapper.kt
@@ -6,14 +6,19 @@ import android.util.Log
 import android.view.View
 import android.widget.FrameLayout
 import androidx.lifecycle.Lifecycle
+import com.google.ar.core.Anchor
 import com.google.ar.core.Config
+import com.google.ar.core.Plane
+import com.google.ar.core.TrackingFailureReason
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.platform.PlatformView
 import io.github.sceneview.ar.ARSceneView
-import io.github.sceneview.model.ModelInstance
+import io.github.sceneview.ar.arcore.getUpdatedPlanes
+import io.github.sceneview.ar.node.AnchorNode
+import io.github.sceneview.math.Position
 import io.github.sceneview.node.ModelNode
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -26,13 +31,15 @@ class FlutterArWrapper(
     messenger: BinaryMessenger,
     id: Int,
 ) : PlatformView, MethodCallHandler {
-    private val TAG = "FlutterArWrapper"
+    private val TAG = "SceneViewWrapper"
     private var sceneView: ARSceneView
-    private val _mainScope = CoroutineScope(Dispatchers.Main)
-    private val _channel = MethodChannel(messenger, "scene_view_$id")
+    private val mainScope = CoroutineScope(Dispatchers.Main)
+    private val channel = MethodChannel(messenger, "scene_view_$id")
+
+    private var anchorNode: AnchorNode? = null
+    private var currentTrackingFailureReason: TrackingFailureReason? = null
 
     override fun getView(): View {
-        Log.i(TAG, "getView:")
         return sceneView
     }
 
@@ -44,6 +51,7 @@ class FlutterArWrapper(
         Log.i(TAG, "init")
         sceneView = ARSceneView(context, sharedLifecycle = lifecycle)
         sceneView.apply {
+            planeRenderer.isEnabled = true
             configureSession { session, config ->
                 config.lightEstimationMode = Config.LightEstimationMode.ENVIRONMENTAL_HDR
                 config.depthMode = when (session.isDepthModeSupported(Config.DepthMode.AUTOMATIC)) {
@@ -52,82 +60,54 @@ class FlutterArWrapper(
                 }
                 config.instantPlacementMode = Config.InstantPlacementMode.DISABLED
             }
-            onSessionResumed = { session ->
-                Log.i(TAG, "onSessionCreated")
-            }
-            onSessionFailed = { exception ->
-                Log.e(TAG, "onSessionFailed : $exception")
-            }
-            onSessionCreated = { session ->
-                Log.i(TAG, "onSessionCreated")
+            onSessionUpdated = { _, frame ->
+                if (anchorNode == null) {
+                    frame.getUpdatedPlanes()
+                        .firstOrNull { it.type == Plane.Type.HORIZONTAL_UPWARD_FACING }
+                        ?.let { plane ->
+                            addAnchorNode(plane.createAnchor(plane.centerPose))
+                        }
+                }
             }
             onTrackingFailureChanged = { reason ->
-                Log.i(TAG, "onTrackingFailureChanged: $reason");
+                if (currentTrackingFailureReason != reason) {
+                    currentTrackingFailureReason = reason
+                    channel.invokeMethod("onTrackingFailureChanged", reason?.name)
+                }
             }
+
         }
         sceneView.layoutParams = FrameLayout.LayoutParams(
             FrameLayout.LayoutParams.MATCH_PARENT,
             FrameLayout.LayoutParams.MATCH_PARENT
         )
         sceneView.keepScreenOn = true
-        _channel.setMethodCallHandler(this)
+        channel.setMethodCallHandler(this)
     }
 
-    private suspend fun addNode(flutterNode: FlutterSceneViewNode) {
-        val node = buildNode(flutterNode) ?: return
-        sceneView.addChildNode(node)
-        //AnchorNode(sceneView.engine, anchor).apply {}
-        Log.d("Done", "Done")
+    private fun addAnchorNode(anchor: Anchor) {
+        sceneView.addChildNode(
+            AnchorNode(sceneView.engine, anchor).apply {
+                isEditable = true
+                mainScope.launch {
+                    buildModelNode()?.let { addChildNode(it) }
+                }
+                anchorNode = this
+            }
+        )
     }
 
-    private suspend fun buildNode(flutterNode: FlutterSceneViewNode): ModelNode? {
-        var model: ModelInstance? = null
-
-        /*
-                AnchorNode(sceneView.engine, anchor)
-                    .apply {
-                        isEditable = true
-                        //isLoading = true
-                        sceneView.modelLoader.loadModelInstance(
-                            "https://sceneview.github.io/assets/models/DamagedHelmet.glb"
-                        )?.let { modelInstance ->
-                            addChildNode(
-                                ModelNode(
-                                    modelInstance = modelInstance,
-                                    // Scale to fit in a 0.5 meters cube
-                                    scaleToUnits = 0.5f,
-                                    // Bottom origin instead of center so the model base is on floor
-                                    centerOrigin = Position(y = -0.5f)
-                                ).apply {
-                                    isEditable = true
-                                }
-                            )
-                        }
-                        //isLoading = false
-                        anchorNode = this
-                    }
-        */
-        when (flutterNode) {
-            is FlutterReferenceNode -> {
-                val fileLocation = Utils.getFlutterAssetKey(activity, flutterNode.fileLocation)
-                Log.d("FlutterArWrapper", fileLocation)
-                model =
-                    sceneView.modelLoader.loadModelInstance(fileLocation)
+    private suspend fun buildModelNode(): ModelNode? {
+        sceneView.modelLoader.loadModelInstance(
+            "https://sceneview.github.io/assets/models/DamagedHelmet.glb"
+        )?.let { modelInstance ->
+            return ModelNode(
+                modelInstance = modelInstance,
+                scaleToUnits = 0.5f,
+                centerOrigin = Position(y = -0.5f)
+            ).apply {
+                isEditable = true
             }
-        }
-        if (model != null) {
-            val modelNode = ModelNode(modelInstance = model, scaleToUnits = 1.0f).apply {
-                transform(
-                    position = flutterNode.position,
-                    rotation = flutterNode.rotation,
-                    //scale = flutterNode.scale,
-                )
-                //scaleToUnitsCube(flutterNode.scaleUnits)
-                // TODO: Fix centerOrigin
-                //     centerOrigin(Position(x=-1.0f, y=-1.0f))
-                //playAnimation()
-            }
-            return modelNode
         }
         return null
     }
@@ -137,17 +117,11 @@ class FlutterArWrapper(
             "init" -> {
                 result.success(null)
             }
-
             "addNode" -> {
                 Log.i(TAG, "addNode")
-                val flutterNode = FlutterSceneViewNode.from(call.arguments as Map<String, *>)
-                _mainScope.launch {
-                    addNode(flutterNode)
-                }
+                // This method is no longer needed as we're automatically placing the model
                 result.success(null)
-                return
             }
-
             else -> result.notImplemented()
         }
     }
